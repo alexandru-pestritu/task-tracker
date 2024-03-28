931dc6c94feb627bd3fa6c7026ffcd40f52627a2
alexandru-pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2024-03-28 10:51:39 +0200

    Implemented navigation to add task with routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { TasksViewComponent } from './tasks-view/tasks-view.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, TasksViewComponent],
+  imports: [RouterOutlet, MatToolbarModule, TasksViewComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
